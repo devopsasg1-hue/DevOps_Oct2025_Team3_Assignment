38097fabb4a1d43de6693ea23781b723cfda995d
shar <faethychan@gmail.com>
2026-01-13 14:27:18 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/DevOps_Oct2025_Team3_Assignment/",
+  base: "",
 });
