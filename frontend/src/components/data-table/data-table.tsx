54225f8fb464409b399ea654378f50c7507c5cca
Yilin100 <gongyilin100@gmail.com>
2026-01-17 14:39:50 +0800

    finished dropdown menu

--- frontend/src/components/data-table/data-table.tsx
+++ frontend/src/components/data-table/data-table.tsx
@@ -32,48 +32,54 @@ export function DataTable<TData, TValue>({
     })
 
     return (
-        <div className="overflow-hidden rounded-md border border-gray-300 shadow-sm w-5/6 h-[550px]">
-            <Table className={undefined}>
-                <TableHeader className={undefined}>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id} className={undefined}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id} className={undefined}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                    </TableHead>
-                                )
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody className={undefined}>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow
-                                key={row.id}
-                                data-state={row.getIsSelected() && "selected"} className={undefined}                            >
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id} className={undefined}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
+        <div className="flex flex-col w-full h-full gap-4">
+            <div className="flex-2/3 flex col overflow-hidden rounded-md border border-gray-300 shadow-sm ">
+                <Table className={undefined}>
+                    <TableHeader className="sticky top-0 z-10 bg-background shadow-md">
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id} className={undefined}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id} className={undefined}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    )
+                                })}
                             </TableRow>
-                        ))
-                    ) : (
-                        <TableRow className={undefined}>
-                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                No results.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
+                        ))}
+                    </TableHeader>
+                    <TableBody className={undefined}>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && "selected"} className={undefined}                            >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id} className={undefined}>
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow className={undefined}>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex-1 text-sm text-muted-foreground">
+                {table.getSelectedRowModel().rows.length} of {}
+                {table.getRowModel().rows.length} rows selected
+            </div>
         </div>
     )
 }
--- frontend/src/components/data-table/page.tsx
+++ frontend/src/components/data-table/page.tsx
@@ -4,6 +4,15 @@ import {useEffect, useState} from "react";
 import {createColumnHelper} from "@tanstack/react-table";
 import axios from "axios";
 import {Checkbox} from "@/components/ui/checkbox";
+import {
+    DropdownMenu,
+    DropdownMenuContent, DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import {Button} from "@/components/ui/button";
+import {MoreVertical} from "lucide-react";
 
 
 
@@ -39,8 +48,43 @@ const columns = [
         cell:(info) => info.getValue(),
     }),
     columnHelper.accessor("created_at",{
-        header: () => <p>Create At</p>,
+        header: () => <p>Created At</p>,
         cell: (info) => info.getValue()
+    }),
+    columnHelper.display({
+        id:"more",
+        cell: ({row}) => {
+
+            const user = row.original; // Get the user object for this row
+
+            const handleDelete = () => {
+                console.log('Delete clicked for user:', user);
+                // Add your delete logic here
+            };
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant={"ghost"} className={undefined}>
+                            <MoreVertical/>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent onCloseAutoFocus={(e: { preventDefault: () => any; })=> e.preventDefault()} className={undefined}>
+                        <DropdownMenuLabel className={undefined} inset={undefined}>
+                            Actions
+                        </DropdownMenuLabel>
+                        <DropdownMenuSeparator className={undefined}/>
+                        <DropdownMenuItem
+                            onClick={handleDelete}
+
+                            className={undefined}
+                            inset={undefined}
+
+                        >Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        }
     })
 ]
 
